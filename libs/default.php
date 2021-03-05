7d627a9e8c659747008451f205985f8d606544f4
RobinStone <vohuanrok@mail.ru>
2021-03-05 22:18:41 +0300

    Собрал проект по шаблонизаторам и добавил функции безопастности в ./libs/default.php

--- libs/default.php
+++ libs/default.php
@@ -8,3 +8,46 @@ function format_cost($cost=0) {
     return $ans;
 }
 
+spl_autoload_register(function ($class) {
+    include './libs/class_'.$class.'.php';
+});
+
+function wtf($array, $stop = false) {
+    echo '<pre style="font-size: 13px; line-height: 16px">'.print_r($array,1).'</pre>';
+    if(!$stop) {
+        exit();
+    }
+}
+
+function out_secur($var) {
+    if(!is_array($var)) {
+        $var = htmlspecialchars($var);
+    } else {
+        $var = array_map('out_secur', $var);
+    }
+    return $var;
+}
+
+function db_secur($var, $key = 0) {
+    if(!is_array($var)) {
+        $var = DB::_($key)->real_escape_string($var);
+    } else {
+        $var = array_map('db_secur', DB::_($key)->real_escape_string($var));
+    }
+    return $var;
+}
+
+function trimAll($var) {
+    if(!is_array($var)) {
+        $var = trim($var);
+    } else {
+        $var = array_map('trimAll', $var);
+    }
+    return $var;
+}
+
+function crypter($var) {
+    $salt1 = 'v5kmmge423tttrmgpo5g7h40';
+    $salt2 = 'fn21kfkr95legdf32ls657v';
+    return crypt(md5($salt2.$var.$salt1), $salt2);
+}
--- templates/main.php
+++ templates/main.php
@@ -1,1 +1,42 @@
-<?php
+<main class="container">
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <?php foreach($cats as $v): ?>
+                <!--заполните этот список из массива категорий-->
+                <li class="promo__item promo__item--boards">
+                    <a class="promo__link" href="pages/all-lots.html"><?=out_secur($v)?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <!--заполните этот список из массива с товарами-->
+            <?php foreach($two_cats as $k=>$v): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=out_secur($v['img'])?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=out_secur($v['cat'])?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=out_secur($k)?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount"><?=out_secur($v['cost'])?></span>
+                                <span class="lot__cost"><?=format_cost($v['cost'])?></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+</main>
