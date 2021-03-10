465068e512f785519de76d46e7b8c68247ee905f
RobinStone <vohuanrok@mail.ru>
2021-03-10 23:48:02 +0300

    Работа со ставками начало

--- libs/default.php
+++ libs/default.php
@@ -20,6 +20,13 @@ function format_cost($cost=0) {
     $ans .=' $';  // рубль плохо отображается даже у меня, заменил на $
     return $ans;
 }
+function format_cost_count($cost=0) {
+    $ans = ceil($cost);
+    if($ans >= 1000) {
+        $ans = number_format($cost, 0, '.', ' ');
+    }
+    return $ans;
+}
 
 function q($query, $key = 0) {
     $res = DB::_($key)->query($query);
