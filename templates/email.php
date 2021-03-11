d06b797fc9d6831a6b988262406ebf4f829d06f3
RobinStone <vohuanrok@mail.ru>
2021-03-11 23:02:00 +0300

    Реализовал показ адреса владельца лот у ставки, которая выиграла

---
+++ templates/email.php
@@ -0,0 +1,6 @@
+<h1>Поздравляем с победой</h1>
+<p>Здравствуйте, <?=$arr['login']?></p>
+<p>Ваша ставка для лота <a href="https://yeticave.local/lot/<?=$arr['lot_id']?>"><?=$arr['lot_name']?></a> победила.</p>
+<p>Перейдите по ссылке <a href="https://yeticave.local/my-bets?>">мои ставки</a>,
+    чтобы связаться с автором объявления</p>
+<small>Интернет Аукцион "YetiCave"</small>
--- templates/my-bets.php
+++ templates/my-bets.php
@@ -1,6 +1,6 @@
 <?php
 $ask = q("
-SELECT rates.*, users.id AS ID_USER, lotes.id AS ID_LOTES, lotes.name, lotes.cat, lotes.img, lotes.text, lotes.date_end, lotes.id_winer, lotes.sended, categorys.id AS ID_CAT, categorys.name AS CAT_NAME  FROM `rates`
+SELECT rates.*, users.id AS ID_USER, lotes.id AS ID_LOTES, lotes.name, lotes.cat, lotes.img, lotes.text, lotes.date_end, lotes.id_winer, lotes.id_parent, lotes.sended, categorys.id AS ID_CAT, categorys.name AS CAT_NAME  FROM `rates`
 LEFT JOIN `users` ON rates.id_user = users.id
 LEFT JOIN `lotes` ON rates.id_lot = lotes.id
 LEFT JOIN `categorys` ON lotes.cat = categorys.id
@@ -10,8 +10,25 @@ WHERE rates.id_user = ".(int)$_SESSION['user']['id']." ORDER BY lotes.date_end D
 if($ask->num_rows) {
     while($row = $ask->fetch_assoc()) {
         $arr[] = $row;
+        $id_arr[$row['id']] = ' `id` = '.$row['id_parent'];
     }
 }
+
+if(isset($id_arr, $arr)) {
+    $ask = q("
+    SELECT users.id, users.descr FROM `users` WHERE ".implode(' OR ', $id_arr)."
+    ");
+    if($ask->num_rows) {
+        while($row = $ask->fetch_assoc()) {
+            $descr[$row['id']] = $row['descr'];
+        }
+        foreach($arr as $k=>$v) {
+            $arr[$k]['address'] = $descr[$v['id_parent']];
+        }
+    }
+}
+
+
 ?>
 
 <main>
@@ -32,7 +49,10 @@ if($ask->num_rows) {
                     <div class="rates__img">
                         <img src="../<?=$v['img']?>" width="54" height="40" alt="<?=$v['name']?>">
                     </div>
-                    <h3 class="rates__title"><a href="/lot/<?=(int)$v['ID_LOTES']?>"><?=$v['name']?></a></h3>
+                    <div>
+                        <h3 class="rates__title"><a href="/lot/<?=(int)$v['ID_LOTES']?>"><?=$v['name']?></a></h3>
+                        <?php if($v['id_winer'] == $v['id']) { echo '<p>'.$v['address'].'</p>'; } ?>
+                    </div>
                 </td>
                 <td class="rates__category">
                     <?=$v['CAT_NAME']?>
