993e4942f11719c54d46f108fc4ace8fea4f39dc
RobinStone <vohuanrok@mail.ru>
2021-03-21 19:28:50 +0300

    Убрал неиспользуемые файлы картинки и JS скрипт перенёс в один шаблонов

--- templates/lot.php
+++ templates/lot.php
@@ -57,5 +57,20 @@
     </section>
   </main>
 
-<script src="../rbs.js"></script>
+<script>
+    el = document.getElementById('cost');
+    el.addEventListener('keydown', function(e) {
+        console.log(e.key);
+        let arr = [1,2,3,4,5,6,7,8,9,0];
+        if(e.key !== 'Backspace' && e.key !== 'Enter' && !arr.includes(parseInt(e.key))) {
+            e.preventDefault();
+            return false;
+        }
+        let i = Number(this.value);
+        if(i.lenght > 3) {
+            console.log('ok');
+        }
+
+    }, false);
+</script>
 
